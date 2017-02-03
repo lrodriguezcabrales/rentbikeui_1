5c0ea4ac56a53da24ccee96e0af3e2cde77654bd
lrodriguez <lrodriguez@arajoysegovia.net>
2017-02-02 23:27:03 -0500

    list users

--- js/script.js
+++ js/script.js
@@ -1,22 +1,55 @@
-server = 'http://localhost:8080/';
+server = 'http://localhost/';
 
-function login() {
+
+function login(event) {
 	
-	debugger
+	
+
 	var email = document.getElementById('email').value;
 	
 	var password = document.getElementById('password').value;
 	
+	var data = {
+		"email": email,
+		"password": password
+	};
+
+	data = JSON.stringify(data);
+	//data.push('email', email);
+
+	debugger
+
 	var parameters = {
         type: 'POST',          
         url : server+'rentbike/web/app_dev.php/login',
         contentType: 'application/json',
-        dataType: "json",           
+        dataType: "json",
+        data: data,  
         success: function(response){    
-        	
-			debugger	
+        	if(response.status != 200){
+        		alert(response.msj);
+        	}else{
+
+        		$('#form-login').hide();
+
+        		var divUsers = $('<div class= "users">Usuarios</div>');
+
+        		var divBikes = $('<div class= "bikes">Bicicletas</div>');
+
+        		var divRents = $('<div class= "rents">Rentas</div>');
+
+        		$('.login').append(divUsers);
+
+        		$('.login').append(divBikes);
+
+        		$('.login').append(divRents);
+        		//var table = $('<table></table>');
+
+        	}
+
         },
         error: function (e) {
+        	debugger
 			console.log(e)
 			alert('Ha ocurrido un error'); 
         }
